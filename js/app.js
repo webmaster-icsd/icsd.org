6ef230b52662361942e02bdf35eb62f75c6d074c
Webmaster ICSD <webmaster@icsd.org>
2018-03-09 16:32:29 -0800

    layout chnages add full width layout add full calendar with styling

--- js/app.js
+++ js/app.js
@@ -1,14 +1,59 @@
+var GOOGLE_CALENDAR_ID = 'icsd.org_p8flfo1mj6nftembusod09qj6s@group.calendar.google.com';
+var GOOGLE_API_KEY = 'AIzaSyDzV4tcu-80oWSp0MsR5r2CFD1i6PgSTys';
+
 $(function () {
     'use strict'
-    // var GOOGLE_CALENDAR_ID = 'sisni95sgv8ifcq5su0d8j4khs%40group.calendar.google.com';
-    var GOOGLE_CALENDAR_ID = 'km189pj4a5bptkna3f5gta4rgk@group.calendar.google.com';
-    var GOOGLE_API_KEY = 'AIzaSyDzV4tcu-80oWSp0MsR5r2CFD1i6PgSTys';
+    var upcomingEvents = $('#upcoming-events');
+    if (upcomingEvents.length) {
+        loadUpcomingEvents();
+    }
+    var eventsCalendar = $("#event-calendar");
+    if (eventsCalendar.length) {
+        eventsCalendar.fullCalendar({
+            googleCalendarApiKey: GOOGLE_API_KEY,
+            events: {
+                googleCalendarId: GOOGLE_CALENDAR_ID
+            },
+            eventRender: function(event, element) {
+                element.popover({
+                    html:true,
+                    title:event.title,
+                    placement:'right',
+                    content: "<p>"+ event.description||"" +"</p>",
+                    trigger: 'focus'}
+                ).popover('show');
+            },
+            eventClick: function(event, jsEvent, view) {
+                return false;
+            },
+            themeSystem: 'bootstrap4',
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay,listMonth'
+            },
+            eventLimit: true, // allow "more" link when too many events
+            buttonText : {
+                today:    'Today',
+                month:    'Month',
+                week:     'Week',
+                day:      'Day',
+                list:     'List'
+            },
+            bootstrapFontAwesome:{
+                prev: 'la la la-angle-left',
+                next: 'la la la-angle-right'
+            }
+
+        })
+    }
+});
+
+var loadUpcomingEvents = function () {
     var calendarEventTemplateHtml = $('#calender-event-template').html();
     var calendarEventHtml;
+
     $.ajax({
-        // GET https://www.googleapis.com/calendar/v3/calendars/sisni95sgv8ifcq5su0d8j4khs%40group.calendar.google.com/events?maxResults=25&orderBy=startTime&singleEvents=true&timeMin=2018-02-16T10%3A00%3A00-07%3A00&fields=description%2Citems(description%2Cend%2Crecurrence%2Csource%2Cstart%2Cstatus%2Csummary)%2Csummary&key={YOUR_API_KEY}
-        // url: "https://www.googleapis.com/calendar/v3/calendars/sisni95sgv8ifcq5su0d8j4khs@group.calendar.google.com/events?key=AIzaSyDzV4tcu-80oWSp0MsR5r2CFD1i6PgSTys",
-        // url: "https://www.googleapis.com/calendar/v3/calendars/sisni95sgv8ifcq5su0d8j4khs%40group.calendar.google.com/events?maxResults=25&orderBy=startTime&singleEvents=true&timeMin=2018-02-16T10%3A00%3A00-07%3A00&fields=description%2Citems(colorId%2Ccreator(displayName%2Cself)%2Cdescription%2Cend%2FdateTime%2CendTimeUnspecified%2Cetag%2Cid%2Clocation%2Corganizer%2FdisplayName%2Crecurrence%2Csource%2Cstart%2FdateTime%2Cstatus%2Csummary)%2Csummary&key=AIzaSyDzV4tcu-80oWSp0MsR5r2CFD1i6PgSTys",
         url: "https://www.googleapis.com/calendar/v3/calendars/" + GOOGLE_CALENDAR_ID + "/events",
         data: {
             maxResults: 5,
@@ -22,9 +67,9 @@ $(function () {
             var dataArray = result.items;
             var eventStartDateTime, eventTime;
             for (var i = 0; i < dataArray.length; i++) {
-                if(dataArray[i]["start"]["dateTime"]){
+                if (dataArray[i]["start"]["dateTime"]) {
                     eventStartDateTime = moment(dataArray[i]["start"]["dateTime"]);
-                    if(dataArray[i]["end"]["dateTime"]){
+                    if (dataArray[i]["end"]["dateTime"]) {
                         eventTime = eventStartDateTime.format('h:mma') + " - " + moment(dataArray[i]["end"]["dateTime"]).format('h:mma');
                     } else {
                         eventTime = eventStartDateTime.format('h:mma');
@@ -32,16 +77,16 @@ $(function () {
                 } else {
                     eventStartDateTime = moment(dataArray[i]["start"]["date"]);
                 }
-                calendarEventHtml = calendarEventTemplateHtml.replace(/##event-day##/g, eventStartDateTime.format('DD') ||'')
-                    .replace(/##event-month##/g, eventStartDateTime.format('MMM') ||'')
-                    .replace(/##event-title##/g, dataArray[i]["summary"] ||'')
-                    .replace(/##event-description##/g, dataArray[i]["description"] ||'')
-                    .replace(/##event-weekday##/g, eventStartDateTime.format('ddd') ||'')
-                    .replace(/##event-time##/g, eventTime  ||'')
-                    .replace(/##event-location##/g, dataArray[i]["location"]||'Islamic Center Of San Diego');
+                calendarEventHtml = calendarEventTemplateHtml.replace(/##event-day##/g, eventStartDateTime.format('DD') || '')
+                    .replace(/##event-month##/g, eventStartDateTime.format('MMM') || '')
+                    .replace(/##event-title##/g, dataArray[i]["summary"] || '')
+                    .replace(/##event-description##/g, dataArray[i]["description"] || '')
+                    .replace(/##event-weekday##/g, eventStartDateTime.format('ddd') || '')
+                    .replace(/##event-time##/g, eventTime || '')
+                    .replace(/##event-location##/g, dataArray[i]["location"] || 'Islamic Center Of San Diego');
                 $('#upcoming-events').append(calendarEventHtml);
             }
         }
     });
-});
+};
 
